7448b14090e76507e6bbf839379562a1c4c83853
Gorzu <gorzelnik.p@gmail.com>
2020-12-02 19:28:02 +0100

    Początek zadania 2. Inicjalizacja kodem z z1

---
+++ Lab5/z2.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFFER 1000
+
+int main(int argc, char const *argv[])
+{
+	int pid = fork();
+	int tab[2];
+
+	if(pid>=0)
+	{
+		const char* filename = argv[1];
+		FILE *fptr;
+		if((fptr = fopen(filename, "r")) == NULL)
+		{
+			fprintf(stderr, "%s\n", "Błąd otwarcia pliku");
+			return 1;
+		}
+
+		char buffer[BUFFER];
+		fgets(buffer, BUFFER, fptr);
+		printf("Proces nadrzędny: %s", buffer);
+		fclose(fptr);
+		write(tab[1], buffer, sizeof(char)*BUFFER);
+		close(tab[1]);
+	}
+
+
+	if(pid==0)
+	{
+		close(tab[1]);
+		char buffer[BUFFER];
+		read(tab[0], buffer, sizeof(char)*BUFFER);
+        printf("Podproces: #%s #", buffer);
+        close(tab[0]);
+	}
+
+	return 0;
+}
