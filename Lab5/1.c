4705aafc5009383f692b7293d9bae17a54379d0b
Kajaja <kkajjaa@gmail.com>
2020-12-02 19:50:28 +0100

    zad 1

--- Lab5/1.c
+++ Lab5/1.c
@@ -9,30 +9,51 @@
 
 int main (int argc, char* argv[])
 {
-
     int potok[2];
-    pipe(potok);
+    int id; //file descriptor
+    char buffer[SIZE];
+    char *filename = argv[1];
+
+    if (pipe(potok) == -1) //utworzenie potoku funkcja pipe
+	{		
+		printf("Blad tworzenia potoku. \n");
+	}
 
     pid_t pid;
 	pid = fork(); //utworzenie podprocesu
-    
+
+    id = open(filename, O_WRONLY | O_APPEND);
+
     if (pid < 0)
-    {
-        //blad
-    }
+ 	{
+		printf("Blad tworzenia procesu potomnego. \n");
+	}
 
     //rodzic - proces nadrzedny
 	else if (pid > 0)
 	{
-        //zamyka koniec do odczytu
+        close(potok[0]); //zamyka koniec potoku do odczytu
+
+        if (argc != 2)
+		{
+			printf("Wymaganie jest podanie jednego argumentu. \n");
+		}
+ 
         //wpisuje writem
+        if (write(id, &buffer, SIZE) = 0) 
+        {
+            {
+				printf("Nie udalo sie zapisac do potoku. \n");
+			}
+ 
+        }
 
     }
 
     //dziecko - proces podrzedny
 	else
 	{
-        //zamyka koniec do wpisywania
+		close(potok[1]); //zamyka koniec potoku do wpisywania
         //odczytuje readem
         //wyswietla na ekran
 
@@ -40,5 +61,6 @@ int main (int argc, char* argv[])
 
 
 }
+
 
 
