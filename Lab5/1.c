c5b8c70e5b4e8881ee4bcdab8bae2096858bc6ad
Kajaja <kkajjaa@gmail.com>
2020-12-02 20:22:52 +0100

    zad 1

--- Lab5/1.c
+++ Lab5/1.c
@@ -10,20 +10,21 @@
 int main (int argc, char* argv[])
 {
     int potok[2];
-    int id; //id - file descriptor
+    int id, k, file; //id - file descriptor
     char buffer[SIZE];
     char *filename = argv[1];
-    int k;
 
     if (pipe(potok) == -1) //utworzenie potoku funkcja pipe
 	{		
 		printf("Blad tworzenia potoku. \n");
 	}
+    else
+	{
+		printf("Potok zostal utworzony. \n");
+	}
 
     pid_t pid;
 	pid = fork(); //utworzenie podprocesu
-
-    id = open(filename, O_WRONLY | O_APPEND);
 
     if (pid < 0)
  	{
@@ -33,6 +34,7 @@ int main (int argc, char* argv[])
     //rodzic - proces nadrzedny
 	else if (pid > 0)
 	{
+        printf("Uruchomiono proces nadrzedny. \n");
         close(potok[0]); //zamyka koniec potoku do odczytu
 
         if (argc != 2)
@@ -41,14 +43,14 @@ int main (int argc, char* argv[])
 		}
         else
         {
-            if((id = open(filename, O_WRONLY)) = 0)
+            if((id = open(filename, O_RDONLY)) < 0)
             {
                 printf("Nie udalo sie otworzyc pliku. \n");
             }
             
-            while ((k = read(id, buffer, SIZE)) > 0 )
+            while ((k = read(id, &buffer, SIZE)) > 0 )
             {
-                if (write(potok[1], &buffer, k) = 0) //wpisywanie do konca potoku
+                if (write(potok[1], &buffer, k) < 0) //wpisywanie do konca potoku
                 {
 				    printf("Nie udalo sie zapisac do potoku. \n");
 			    }
@@ -56,6 +58,7 @@ int main (int argc, char* argv[])
             }
 
             close(id);
+            close(potok[1]); //zamyka koniec potoku do wpisywania
         }
 
     }
@@ -64,15 +67,17 @@ int main (int argc, char* argv[])
 	else
 	{
 		close(potok[1]); //zamyka koniec potoku do wpisywania
+        printf("Uruchomiono podproces. \n");
 
-        while(file = read(potok[0], &buffer, SIZE) = 0) //odczytywanie zawartosci pliku
+        while(file = read(potok[0], &buffer, SIZE) > 0) //odczytywanie zawartosci pliku
         {
-            printf("#%s#\n", buffer);
+            printf("\nZawartosc pliku: \n\n#%s#\n", buffer);
         }
 
+        close(potok[0]); //zamyka koniec potoku do odczytu
     }
 
-
+    return 0;
 }
 
 
