b395ec84ded1fa365cba59dd1328c2ba47dd3eed
Gorzu <gorzelnik.p@gmail.com>
2020-12-16 18:23:21 +0100

    Dodano pobranie statusu pliku i otwarcie drugiego pliku

--- Lab6/z1.c
+++ Lab6/z1.c
@@ -2,13 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-int int main()
+
+int main()
 {
 	int plik1, plik2; // deskryptory
 	char file_name[64];
 	char output_file[] = "output.txt";
+	struct stat status; 
 
+
+	while(1)
+	{
+		printf("Podaj nazwę pliku do odczytu \n");
+		scanf("%s", file_name);
+
+		plik1 = open(file_name, O_RDONLY);
+	
+
+		stat(file_name, &status); // pobranie statusu pliku do struktury
+		plik2 = open(output_file, O_RDWR); // otwarcie 2 pliku przez deskryptor do mapowania
+		truncate(output_file, status.st_size); // utworzenie tablicy o odpowiednim rozmiarze
+	}
 
 	return 0;
 }
