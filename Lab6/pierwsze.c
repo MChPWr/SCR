82cfe03c2bdb9b870b218baba4da2206aed825ca
Kajaja <kkajjaa@gmail.com>
2020-12-16 19:40:52 +0100

    pierwsze

---
+++ Lab6/pierwsze.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int file, file2;
+    char filename[100];
+
+
+    while(1)
+    {
+        printf("Podaj nazwe pliku: \n");
+        scanf("%s", filename);
+
+    }
+
+
+}
