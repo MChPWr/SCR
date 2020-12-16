b1ef74aa13d6fa13d15686a1e31d1e7d70505907
Gorzu <gorzelnik.p@gmail.com>
2020-12-16 18:10:26 +0100

    Początek zadania 1

---
+++ Lab6/z1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+int int main()
+{
+	int plik1, plik2; // deskryptory
+	char file_name[64];
+	char output_file[] = "output.txt";
+
+
+	return 0;
+}
