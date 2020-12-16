1a796a7691a3692f259cc4b53ecb243db06c2e07
Gorzu <gorzelnik.p@gmail.com>
2020-12-16 19:58:22 +0100

    main body

--- Lab6/z2.c
+++ Lab6/z2.c
@@ -10,5 +10,27 @@
 
 int main()
 {
+	char file_name[64];
+	char output_file[] = "balbina.jpeg"; // piękna i zjawiskowa
+	char *adres = NULL;
+	int plik1, plik2;
+	int pid = fork();
+
+	if(pid == 0)
+	{
+		execlp("display", "display", "-update", "1", output_file, NULL);
+	}
+
+
+	else
+	{
+		while(1)
+		{
+			printf("Podaj nazwę pliku do odczytu \n");
+			scanf("%s", file_name);
+			plik1 = open(file_name, O_RDONLY);
+		}
+	}
+
 	return 0;
 }
