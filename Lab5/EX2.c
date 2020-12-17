c703220ee88ccaeaf9ca2b157472d2f1d08f03e9
Patryk Szydlik <patryk.szydlik@tlen.pl>
2020-12-17 08:51:53 +0100

    added fork

--- Lab5/EX2.c
+++ Lab5/EX2.c
@@ -6,29 +6,44 @@
 #include <fcntl.h>  // open()
 #define BUFOR 100
 
-int main(){
-char file_path[BUFOR];                  // zmienna do zapisu podanej przez uzytkownika nazwy 
-struct stat sb;                         // struktura statystyk pliku
-int fd_map=open("mapped_img.txt", O_RDWR);  // file descriptor dla pliku do którego mapujemy pamięć
-int fd_file;                            // file descriptor dla pliku czytanego
-size_t length;                          // rozmiar pamięci do zmapowania
-char *addr;                             // adres zmapowanej pamięci
+const char mapped_filename[15]="mapped_img";
 
-while(1){
-    printf("[EX to exit]   Podaj nazwe pliku do zmapowania: ");
-    fscanf (stdin, "%s", file_path);
-    if(file_path[0]=='E' && file_path[1]=='X' && file_path[2]=='\0'){break;};
-    stat(file_path, &sb);
-    length=sb.st_size;
-    addr=mmap(NULL, length, PROT_READ|PROT_WRITE, MAP_SHARED,fd_map, 0);
-    ftruncate(fd_map, length);
-    fd_file=open(file_path,O_RDONLY|O_CREAT,  S_IRWXU);
-    if (fd_file == -1){ 
-        fprintf(stderr,"ERR: Can't open file to read \n");
-    }else{
-        read(fd_file, addr, length);
+int main(){
+    char file_path[BUFOR];                  // zmienna do zapisu podanej przez uzytkownika nazwy 
+    struct stat sb;                         // struktura statystyk pliku
+    int fd_map=open(mapped_filename, O_RDWR);  // file descriptor dla pliku do którego mapujemy pamięć
+    int fd_file;                            // file descriptor dla pliku czytanego
+    size_t length;                          // rozmiar pamięci do zmapowania
+    char *addr;                             // adres zmapowanej pamięci
+    int pid=0;                              // do kontroli wartości zwracanej przez fork
+
+
+    pid=fork();
+    if(pid<0){                 // sprawdzamy czy fork się powiódł
+        fprintf(stderr, "ERR:  Nie udało się utworzyć procesu dziecka" ); 
+        return -1; 
+
+    ///////////  DZIECKO  /////////////////
+    }else if(pid==0){   
+        
+    ///////////  RODZIC  /////////////////
+    }else{   
+        while(1){
+            printf("[EX to exit]   Podaj nazwe pliku do zmapowania: ");
+            fscanf (stdin, "%s", file_path);
+            if(file_path[0]=='E' && file_path[1]=='X' && file_path[2]=='\0'){break;};
+            stat(file_path, &sb);
+            length=sb.st_size;
+            addr=mmap(NULL, length, PROT_READ|PROT_WRITE, MAP_SHARED,fd_map, 0);
+            ftruncate(fd_map, length);
+            fd_file=open(file_path,O_RDONLY|O_CREAT,  S_IRWXU);
+            if (fd_file == -1){ 
+                fprintf(stderr,"ERR: Nie można otworzyć pliku do odczytu \n");
+            }else{
+                read(fd_file, addr, length);
+            };
+            munmap(addr, length);
+        };
     };
-}
-munmap(addr, length);
-close(fd_map);
+    close(fd_map);
 }
