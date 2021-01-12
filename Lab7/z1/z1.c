5ebf1edeb6c341e24c67afcad95b46b005e5f837
Gorzu <gorzelnik.p@gmail.com>
2021-01-12 14:25:23 +0100

    Dodanie funkcji WypiszID

--- Lab7/z1/z1.c
+++ Lab7/z1/z1.c
@@ -4,6 +4,13 @@
 
 #define THREAD_NUMBER 8
 
+/* Funkcja do wypisywania ID wątku na standardowym wyjściu */
+void *WypiszID(void threadID){
+	long int ID = (long int) threadID;
+	printf("Hello SCR. Written by thread: %d\n", ID);
+	pthread_exit(NULL); 
+}
+
 
 int int main()
 {
