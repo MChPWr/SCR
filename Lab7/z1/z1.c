9a25f93d84fae58aa57482910282a216cb15f989
Gorzu <gorzelnik.p@gmail.com>
2021-01-12 14:39:42 +0100

    Zadanie 1 gotowe

--- Lab7/z1/z1.c
+++ Lab7/z1/z1.c
@@ -5,20 +5,32 @@
 #define THREAD_NUMBER 8
 
 /* Funkcja do wypisywania ID wątku na standardowym wyjściu */
-void *WypiszID(void threadID){
+void *WypiszID(void *threadID){
 	long int ID = (long int) threadID;
-	printf("Hello SCR. Written by thread: %d\n", ID);
-	pthread_exit(NULL); 
+	printf("Hello SCR. Written by thread: %ld\n", ID);
+	pthread_exit(NULL); // zakończenie wątku
 }
 
 
-int int main()
+int main()
 {
 	pthread_t threads[THREAD_NUMBER]; // tablica wątków
 	int result;
-	long int ID;
+	long int threadID;
 
+	/* Tworzenie kolejnych wątków i wypisanie ich w konsoli */
+	for(threadID=1; threadID<THREAD_NUMBER; threadID++)
+	{
+		result = pthread_create(&threads[threadID], NULL, WypiszID, (void *)threadID);
+	}
 
+	if(result != 0)
+	{
+		printf("ERROR %d\n", result);
+		exit(-1);
+	}
+
+	pthread_exit(NULL); // ostatnia czynność głównego programu głównego
 
 	return 0;
 }
