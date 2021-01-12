e65d92996b82b3ad0f3e401bbdc406220b009897
Gorzu <gorzelnik.p@gmail.com>
2021-01-12 14:11:29 +0100

    Początkek zadania 1

---
+++ Lab7/z1/z1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define THREAD_NUMBER 8
+
+
+int int main()
+{
+	pthread_t threads[THREAD_NUMBER]; // tablica wątków
+	int result;
+	long int ID;
+
+
+
+	return 0;
+}
