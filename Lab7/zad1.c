5355c61e5d75afd58d1b4131d86c58cc28b9cfca
Michal <mbagnsk@hotmail.com>
2021-01-13 17:59:38 +0100

    3.0

---
+++ Lab7/zad1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUM_THREADS 10
+
+void *PrintHello(void  *threadid){
+	long tid;
+	tid = (long)threadid;
+	printf("hello %d\n", tid);
+	pthread_exit(NULL);
+}
+
+int main(){
+	pthread_t threads[NUM_THREADS];
+	int rn;
+	long t;
+
+	for(t = 0; t < NUM_THREADS; t++){
+		rn = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+	}
+	pthread_exit(NULL);
+}
