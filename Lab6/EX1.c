e059cca421bbc1685cbe4e593e830547ab258c55
Patryk Szydlik <p_szydlik@pop-os.localdomain>
2021-01-13 20:04:03 +0100

    EX1 jakos dziala

--- Lab6/EX1.c
+++ Lab6/EX1.c
@@ -1,8 +1,26 @@
 #include <pthread.h>
 #include <stdio.h>
 
+#define N 20
+
+/* funkcja wykonywana w wątku */
+void* thread_func(void* thread_ID) {
+    int * ID = (int * ) thread_ID;
+	printf("Hello SCR. Written by thread %d \n", *ID);
+    pthread_exit(NULL);
+	return NULL;
+}
+
 int main(){
 
+    pthread_t thread_ar[N];
 
+    for(int i=0;i<N;i++){
+        if(pthread_create(&thread_ar[i], NULL, thread_func, &thread_ar[i])) {
+            fprintf(stderr, "Error creating thread\n");
+            return 1;
+        }
+    }
 
+    pthread_exit(NULL);
 }
